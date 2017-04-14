5acce27e9f94958ee9f5d919b91bb204580aee55
lastunicorn <coldblackstar@yahoo.com>
2017-04-14 13:58:09 +0300

    added a common assembly info file.

--- sources/AssemblyInfo.Shared.cs
+++ sources/AssemblyInfo.Shared.cs
@@ -15,20 +15,25 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsolePlus")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Dust in the Wind")]
+[assembly: AssemblyProduct("Console Plus")]
+[assembly: AssemblyCopyright("Copyright © Dust in the Wind 2017")]
+[assembly: AssemblyTrademark("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("45a59c28-a501-4cfe-83d0-edb0d3401ca2")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
--- sources/HashSafe/Properties/AssemblyInfo.cs
+++ sources/HashSafe/Properties/AssemblyInfo.cs
@@ -23,10 +23,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HashSafe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HashSafe")]
-[assembly: AssemblyCopyright("Copyright © Dust in the Wind 2017")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -36,16 +32,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("03ab9e33-50ae-4c9b-a856-3364b6c9fb7c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
